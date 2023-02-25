23517131d456ca55a8d962023f13dfedca9fae55
KingOfParanaue <maateus.lopes.nv@hotmail.com>
2023-02-25 16:49:46 -0300

    Criação do listar transacoes

--- src/controladores/transacoes.js
+++ src/controladores/transacoes.js
@@ -3,6 +3,13 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const senhaJwt = require('../senhaJwt');
 
-module.exports = {
+const listarTransacoes = async (req, res) => {
+    const transacoesUsuario = await pool.query(`SELECT t.id, t.tipo, t.descricao, t.valor, t.data, t.usuario_id, t.categoria_id, c.descricao as categoria_nome 
+    FROM transacoes t JOIN categorias c ON t.usuario_id = c.id WHERE t.usuario_id = $1`, [tokenUsuario.id]);
 
+    return res.status(200).json(transacoesUsuario.rows)
+}
+
+module.exports = {
+    listarTransacoes
 }
