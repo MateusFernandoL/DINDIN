e7b5f16ae03970031938873168529962264d00a5
Pedro Melo <pedromarcosprado26@gmail.com>
2023-02-27 21:54:14 -0300

    Correções

--- src/sql/dump.sql
+++ src/sql/dump.sql
@@ -1,43 +1,48 @@
 -- Tabelas
 create database dindin;
 
-CREATE TABLE usuarios (
-	id serial PRIMARY KEY,
+CREATE TABLE usuarios
+(
+    id serial PRIMARY KEY,
     nome text,
     email text UNIQUE,
     senha text
 );
 
-CREATE TABLE categorias (
-	id serial PRIMARY KEY,
+CREATE TABLE categorias
+(
+    id serial PRIMARY KEY,
     descricao text
 );
 
-CREATE TABLE transacoes (
+CREATE TABLE transacoes
+(
     id serial PRIMARY KEY,
+    tipo text,
     descricao text,
     valor integer,
     data date,
     categoria_id serial REFERENCES categorias(id),
     usuario_id serial REFERENCES usuarios(id),
-    tipo text
 );
 
-INSERT INTO categorias (descricao) VALUES 
-('Alimentação'),
-('Assinaturas e Serviços'),
-('Casa'),
-('Mercado'),
-('Cuidados Pessoais'),
-('Educação'),
-('Família'),
-('Lazer'),
-('Pets'),
-('Presentes'),
-('Roupas'),
-('Saúde'),
-('Transporte'),
-('Salário'),
-('Vendas'),
-('Outras receitas'),
-('Outras despesas');
+INSERT INTO categorias
+    (descricao)
+VALUES
+    ('Alimentação'),
+    ('Assinaturas e Serviços'),
+    ('Casa'),
+    ('Mercado'),
+    ('Cuidados Pessoais'),
+    ('Educação'),
+    ('Família'),
+    ('Lazer'),
+    ('Pets'),
+    ('Presentes'),
+    ('Roupas'),
+    ('Saúde'),
+    ('Transporte'),
+    ('Salário'),
+    ('Vendas'),
+    ('Outras receitas'),
+    ('Outras despesas');
